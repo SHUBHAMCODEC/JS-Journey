4d70af1a1cad16380bff9151cea813a76d14c1a7
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2024-10-15 07:25:16 +0000

    commit

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+// in js we have 3 ways to declare variables 1.By "const" keyword  2. By "let" keyword  3. By "var" keyword 
+
+const schoolId=1249; // this cant be change as this initialise by const keyword 
+let myName="shubham sharma"; // we mostly time we use this initialisation in js
+var mybrotherName="shivam sharma" // in this we face scope problem 
+ 
+
+console.table([schoolId,myName,mybrotherName]);
+
+
+
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js ode as newer version 
+
+// alert(3+3) // error will occur  ass wea are using nodejs, not browser
+
+console.log(3+3);
+
+let namee="shubham"; //string => use either single quotes ('') or double quotes("")
+let age=19; //number
+let isLoggedIn=false;
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+console.table([typeof namee, typeof age, typeof isLoggedIn]);
+
+
+//
+//null => standalone value
+//undefined => nothing
+//symbol => unique 
+
+// object
+
